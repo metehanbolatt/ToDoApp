9b4f4296bbd12c335c3a6616441fd3a0a4e2c351
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-02 23:10:59 +0300

    Dagger-Hilt was created.

--- app/src/main/java/com/metehanbolat/todoappcompose/data/ToDoDatabase.kt
+++ app/src/main/java/com/metehanbolat/todoappcompose/data/ToDoDatabase.kt
@@ -6,6 +6,7 @@ import com.metehanbolat.todoappcompose.data.models.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
 abstract class ToDoDatabase: RoomDatabase() {
+
     abstract fun toDoDao(): ToDoDao
 
 }
---
+++ app/src/main/java/com/metehanbolat/todoappcompose/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.metehanbolat.todoappcompose.di
+
+import android.content.Context
+import androidx.room.Room
+import com.metehanbolat.todoappcompose.data.ToDoDatabase
+import com.metehanbolat.todoappcompose.util.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ToDoDatabase) = database.toDoDao()
+
+}
