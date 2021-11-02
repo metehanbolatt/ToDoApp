4439eff6b32da25d8ad4eeb4a59a204d4be0965c
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-02 23:20:26 +0300

    ToDoRepository has been created.

--- app/src/main/java/com/metehanbolat/todoappcompose/data/ToDoDao.kt
+++ app/src/main/java/com/metehanbolat/todoappcompose/data/ToDoDao.kt
@@ -26,7 +26,7 @@ interface ToDoDao {
     suspend fun deleteAllTasks()
 
     @Query("SELECT * FROM todo_table WHERE title LIKE :searchQuery OR description LIKE :searchQuery")
-    fun searchDatabase(searchQuery: String): Flow<ToDoTask>
+    fun searchDatabase(searchQuery: String): Flow<List<ToDoTask>>
 
     @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
     fun sortByLowPriority(): Flow<List<ToDoTask>>
