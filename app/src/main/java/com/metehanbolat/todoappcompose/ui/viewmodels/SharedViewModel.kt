f5099734a1ff293acc74518cec8e5511b922a4ff
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-08 18:55:39 +0300

    CRUD Operations - Undo Deleted Task

--- app/src/main/java/com/metehanbolat/todoappcompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/metehanbolat/todoappcompose/ui/viewmodels/SharedViewModel.kt
@@ -110,7 +110,7 @@ class SharedViewModel @Inject constructor(private val repository: ToDoRepository
 
             }
             Action.UNDO -> {
-
+                addTask()
             }
             else -> {
 
