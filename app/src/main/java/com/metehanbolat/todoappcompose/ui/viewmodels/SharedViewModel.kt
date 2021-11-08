65d71f50df279ff96bee11561176b177300e16bd
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-08 18:41:57 +0300

    CRUD Operations - Update Task

--- app/src/main/java/com/metehanbolat/todoappcompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/metehanbolat/todoappcompose/ui/viewmodels/SharedViewModel.kt
@@ -71,13 +71,25 @@ class SharedViewModel @Inject constructor(private val repository: ToDoRepository
         }
     }
 
+    private fun updateTask(){
+        viewModelScope.launch(Dispatchers.IO) {
+            val toDoTask = ToDoTask(
+                id = id.value,
+                title = title.value,
+                description = description.value,
+                priority = priority.value
+            )
+            repository.updateTask(toDoTask = toDoTask)
+        }
+    }
+
     fun handleDatabaseActions(action: Action){
         when(action){
             Action.ADD -> {
                 addTask()
             }
             Action.UPDATE -> {
-
+                updateTask()
             }
             Action.DELETE -> {
 
