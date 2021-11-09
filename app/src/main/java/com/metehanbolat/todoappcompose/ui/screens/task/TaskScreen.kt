e6165938e4e2c9b3caade93a9a8b2e564baa671f
Metehan Bolat <metehan.bolat.ie@gmail.com>
2021-11-09 03:24:19 +0300

    OnBackPressed bug has been fixed

--- app/src/main/java/com/metehanbolat/todoappcompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/metehanbolat/todoappcompose/ui/screens/task/TaskScreen.kt
@@ -2,9 +2,11 @@ package com.metehanbolat.todoappcompose.ui.screens.task
 
 import android.content.Context
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import com.metehanbolat.todoappcompose.data.models.Priority
 import com.metehanbolat.todoappcompose.data.models.ToDoTask
@@ -22,6 +24,8 @@ fun TaskScreen(
     val priority: Priority by sharedViewModel.priority
 
     val context = LocalContext.current
+    
+    BackHandler(onBackPressed = { navigateToListScreen(Action.NO_ACTION) })
 
     Scaffold(
         topBar = {
@@ -61,4 +65,26 @@ fun TaskScreen(
 
 fun displayToast(context: Context) {
     Toast.makeText(context, "Fields Empty", Toast.LENGTH_SHORT).show()
+}
+
+@Composable
+fun BackHandler(
+    backDispatcher: OnBackPressedDispatcher? = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit
+) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+    val backCallBack = remember {
+        object : OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+    DisposableEffect(key1 = backDispatcher){
+        backDispatcher?.addCallback(backCallBack)
+        onDispose {
+            backCallBack.remove()
+        }
+    }
+
 }
